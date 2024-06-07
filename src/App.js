eeaa9658a1215f6b29b823e8a58eae8f9fb98173
Robert <Roberttsax18@gmail.com>
2024-06-07 19:20:40 -0400

    much better

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-
+import React, { useState } from 'react';
 import './App.css';
-import HitButton from './components/SuitsComponent';
+import HitButton from './components/HitMeButton';
+
 
 function App() {
+  const [currentCard, setCurrentCard] = useState('');
+
+  const handleHit = () => {
+    const newCard = generateCard();
+    setCurrentCard(newCard);
+  };
+
+  const generateCard = () => {
+    const faces = [  "2",  "3", "4", "5", "6", "7","8",
+      "9","10","Jack","Queen","King","Ace"
+    ];
+    const suits = ["Diamonds", "Hearts", "Clubs", "Spades"];
+
+    const randomFace = faces[Math.floor(Math.random() * faces.length)]
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)]
+
+    return `${randomFace} of ${randomSuit}`;
+  };
+
+
   return (
     <div>
       <h1>BLACKJACK!</h1>
       <div className="hit-me-button">
-      <HitButton/>
+      <HitButton onHit={handleHit}/>
+      </div>
+      <div className="cards">
+        <p>{currentCard}</p>
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/HitMeButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HitButton({ onHit }) {
+    return <button onClick={onHit}>Hit me!</button>;
+}
+
+export default HitButton;
